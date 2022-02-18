1520ede1326da7578de2a66e51c3b08031b1946f
Maximiliano Salibe <salibemaximiliano@gmail.com>
2022-02-17 21:35:42 -0300

    Creé los primeros componentes e instalé Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Encabezado from './componentes/compartidos/Encabezado.js';
+import Principal from './componentes/compartidos/Principal.js';
+import Pie from './componentes/compartidos/Pie.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Encabezado></Encabezado>
+      <Principal></Principal>
+      <Pie></Pie>
     </div>
   );
 }
---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer className="text-red-500">
+        <small>Desarrollado y diseñado por Xavier Reyes Ochoa &copy; 2022 | Todos los derechos reservados.</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
