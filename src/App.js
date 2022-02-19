849537dbed9c34340ce610d1c409632dada48f9a
Maximiliano Salibe <salibemaximiliano@gmail.com>
2022-02-19 18:51:27 -0300

    Completé la lección Metas: HTML del proyecto de React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Encabezado from './componentes/compartidos/Encabezado.js';
 import Principal from './componentes/compartidos/Principal.js';
 import Pie from './componentes/compartidos/Pie.js';
+import Meta from './componentes/lista/Meta';
 
 function App() {
   return (
     <div className="App">
       <Encabezado></Encabezado>
-      <Principal></Principal>
+      <Principal>
+        <Meta></Meta>
+      </Principal>
       <Pie></Pie>
     </div>
   );
---
+++ src/componentes/lista/Meta.js
@@ -0,0 +1,24 @@
+function Meta() {
+    return (
+        <div>
+            <div>
+                <div>icono</div>
+                <div>
+                    <p>1<sub>/ semana</sub></p>
+                    <p>detalles</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>4 de 5</p>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <button>Completado</button>
+            </div>
+        </div>
+    );
+}
+
+export default Meta;
